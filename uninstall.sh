56a09eee59a22df08a8a78918e945c835ec1cbc3
Bruno Gurgel <bruno.gurgel@gmail.com>
2018-06-24 16:57:31 +0000

    Add a simple uninstall file

---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+check_root()
+{
+        if [ "$(whoami)" != "root" ]; then
+                echo "Script must be run as user: root"
+                exit -1
+        fi
+}
+
+rm_files()
+{
+	cd /tmp
+	rm -r /opt/speedflow/
+	rm /etc/sudoers.d/cronflow
+	rm /etc/cron.d/flow*
+}
+
+check_root
+rm_files
+
